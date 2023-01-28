3020b2e1d5e51122881c1c4d9e2dea2bf20d67b5
mrJozhkinKot <j.shchuklina@icloud.com>
2023-01-28 08:46:14 +0300

    feat: add subscribeto and unsubscribefrom function

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -89,12 +89,15 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       if (!user) {
         return reply.code(404).send({message: ERROR_USER_NOT_FOUND})
       }
-      const subscriber = await fastify.db.users.findOne({key:'id', equals: request.body.userId})
-      if (!subscriber) {
-        return reply.code(404).send({message: ERROR_USER_NOT_FOUND})
+      const subscribtion = await fastify.db.users.findOne({key:'id', equals: request.body.userId})
+      if (!subscribtion) {
+        return reply.code(400).send({message: ERROR_USER_NOT_FOUND})
       }
 
-         const updateUser = await fastify.db.users.change(user.id, {subscribedToUserIds: [subscriber.id, ...user.subscribedToUserIds] })
+        await fastify.db.users.change(subscribtion.id, {subscribedToUserIds: [user.id, ...subscribtion.subscribedToUserIds] })
+
+
+         const updateUser = await fastify.db.users.change(user.id, {subscribedToUserIds: [subscribtion.id, ...user.subscribedToUserIds] })
 
       
       return reply.code(200).send(updateUser)
@@ -114,11 +117,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       if (!user) {
         return reply.code(404).send({message: ERROR_USER_NOT_FOUND})
       }
-      const subscriber = await fastify.db.users.findOne({key:'id', equals: request.body.userId})
-      if (!subscriber) {
+      const subscribtion = await fastify.db.users.findOne({key:'id', equals: request.body.userId})
+      if (!subscribtion) {
         return reply.code(404).send({message: ERROR_USER_NOT_FOUND})
       }
-      const updatedUser = await fastify.db.users.change(user?.id, {subscribedToUserIds: user.subscribedToUserIds.filter(subscriberId => subscriberId !== subscriber.id)})
+      const isSubscription = user.subscribedToUserIds.includes(subscribtion.id)
+      const isSubscriber = subscribtion.subscribedToUserIds.includes(user.id)
+      if (!isSubscription || !isSubscriber) {
+        return reply.code(400).send({message: ERROR_USER_NOT_FOUND})
+      }
+      await fastify.db.users.change(subscribtion.id, {subscribedToUserIds: subscribtion.subscribedToUserIds.filter(userId => userId !== user.id)})
+      const updatedUser = await fastify.db.users.change(user.id, {subscribedToUserIds: user.subscribedToUserIds.filter(subscriberId => subscriberId !== subscribtion.id)})
       return reply.code(200).send(updatedUser)
     }
   );
